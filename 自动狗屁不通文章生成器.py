091df8723f0f90ec6e3dffa0758b999307cb9b73
Sunbread <sunbread233@gmail.com>
2019-11-02 03:37:25 +0800

    多方面优化
    - 标点规范化
    - 去除莫名其妙的空格
    - 更标准的自然段格式（开头空格和结尾标点）
    - 避免空行
    - 修复重复生成主题字数次文章的bug
    - 一次读取多个文章主题输入
    - 重构部分代码
    - 可能还有更多（半夜提交记性不好）……

--- 自动狗屁不通文章生成器.py
+++ 自动狗屁不通文章生成器.py
@@ -7,10 +7,10 @@ import random,readJSON
 data = readJSON.读JSON文件("data.json")
 名人名言 = data["famous"] # a 代表前面垫话，b代表后面垫话
 前面垫话 = data["before"] # 在名人名言前面弄点废话
-后面垫话 = data['after']  # 在名人名言后面弄点废话
-废话 = data['bosh'] # 代表文章主要废话来源
-
-xx = "学生会退会"
+后面垫话 = data["after"]  # 在名人名言后面弄点废话
+废话 = data["bosh"] # 代表文章主要废话来源
+句内点号 = data["nonstop"] # 要替换的结尾标点
+句末点号 = data["stop"] # 要保留的结尾标点
 
 重复度 = 2
 
@@ -28,27 +28,35 @@ def 洗牌遍历(列表):
 def 来点名人名言():
     global 下一句名人名言
     xx = next(下一句名人名言)
-    xx = xx.replace(  "a",random.choice(前面垫话) )
-    xx = xx.replace(  "b",random.choice(后面垫话) )
+    xx = xx.replace("a", random.choice(前面垫话))
+    xx = xx.replace("b", random.choice(后面垫话))
     return xx
 
-def 另起一段():
-    xx = ". "
+def 另起一段(段落结尾):
+    xx = 段落结尾
     xx += "\r\n"
-    xx += "    "
+    xx += "　　" # 两个全角空格
     return xx
 
 if __name__ == "__main__":
-    xx = input("请输入文章主题:")
+    xx = input("请输入文章主题（多个以空格分隔）：").split(" ")
     for x in xx:
-        tmp = str()
-        while ( len(tmp) < 6000 ) :
+        print(x + "：")
+        tmp = "　　" # 两个全角空格
+        while len(tmp) < 6000:
             分支 = random.randint(0,100)
             if 分支 < 5:
-                tmp += 另起一段()
-            elif 分支 < 20 :
+                if len(tmp) == 0 or tmp[-1] == "　": pass
+                elif tmp[-1] in 句内点号:
+                    tmp = tmp[:-1]
+                    tmp += 另起一段("。")
+                elif tmp[-1] in 句末点号:
+                    tmp += 另起一段("")
+                else:
+                    tmp += 另起一段("。")
+            elif 分支 < 20:
                 tmp += 来点名人名言()
             else:
                 tmp += next(下一句废话)
-        tmp = tmp.replace("x",xx)
+        tmp = tmp.replace("x",x)
         print(tmp)
