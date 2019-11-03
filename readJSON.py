3a1b7b915ead97187a1344ca8a7607e6a75658e8
Sinhs <pgrty45879@gmail.com>
2019-11-03 15:03:57 +0800

    Update readJSON.py

--- readJSON.py
+++ readJSON.py
@@ -3,10 +3,12 @@
 # @Date:   2019-11-01 16:52:34
 # @Last Modified by:   Admin
 # @Last Modified time: 2019-11-01 18:18:14
+
+import json
+
 def 读JSON文件(fileName=""):
-    import json
-    if fileName!='':
+    if fileName != '':
         strList = fileName.split(".")
         if strList[len(strList)-1].lower() == "json":
-            with open(fileName,mode='r',encoding="utf-8") as file:
+            with open(fileName, mode='r', encoding="utf-8") as file:
                 return json.loads(file.read())
