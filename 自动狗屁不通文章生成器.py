dacd5c0e57840ab410b65db7178a4a316a0ac1dc
elin-croft <wangyilin19941@outlook.com>
2019-11-15 23:59:43 +0800

    fixed encoding bugs

--- 自动狗屁不通文章生成器.py
+++ 自动狗屁不通文章生成器.py
@@ -54,18 +54,18 @@ if __name__ == "__main__":
             exit(1);
         else:
             name = xx + '({}).txt'.format(x)
-    for x in xx:
-        tmp = str()
-        while ( len(tmp) < 6000 ) :
-            分支 = random.randint(0,100)
-            if 分支 < 5:
-                tmp += 另起一段()
-            elif 分支 < 20 :
-                tmp += 来点名人名言()
-            else:
-                tmp += next(下一句废话)
-        tmp = tmp.replace("x",xx)
-        print(tmp)
-        with open(root + name,'a') as f:
+    with open(root + name,'w', encoding='utf-8') as f:
+        for x in xx:
+            tmp = str()
+            while ( len(tmp) < 6000 ) :
+                分支 = random.randint(0,100)
+                if 分支 < 5:
+                    tmp += 另起一段()
+                elif 分支 < 20 :
+                    tmp += 来点名人名言()
+                else:
+                    tmp += next(下一句废话)
+            tmp = tmp.replace("x",xx)
+            print(tmp)
             f.write(tmp)
 
